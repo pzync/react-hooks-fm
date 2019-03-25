353ab8c24eb3bc649f8ef62933d3d0eb42bcdab3
pzync <shkhrgrv@gmail.com>
2019-03-25 10:09:36 +0530

    useDropdown custom hook composed and used

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
+import { ANIMALS } from "petfinder-client";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "Havanese", breeds);
+  // const [animal, setAnimal] = useState("dog");
+  // const [breed, setBreed] = useState("Havanese");
 
   return (
     <div className="search-params">
@@ -15,6 +22,11 @@ const SearchParams = () => {
             onChange={e => setLocation(e.target.value)}
           />
         </label>
+
+        <AnimalDropdown />
+
+        <BreedDropdown />
+
         <button>Submit</button>
       </form>
     </div>
